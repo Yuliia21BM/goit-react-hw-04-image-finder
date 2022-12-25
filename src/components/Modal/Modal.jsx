cc2bfd6c053d6674c1d40a7c1c4c2f346a0ff3ef
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-12-26 00:44:12 +0200

    Add Modal styles

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import { Overlay, ModalImg } from './Modal.styled';
+
+export const Modal = () => {
+  return (
+    <Overlay>
+      <ModalImg>
+        <img src="" alt="" />
+      </ModalImg>
+    </Overlay>
+  );
+};
