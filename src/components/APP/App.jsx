e628eaba8716ba6cf23384b838fe15b43adc79a5
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-01-26 00:36:30 +0200

    Update App.jsx

--- src/components/APP/App.jsx
+++ src/components/APP/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -6,37 +6,32 @@ import { Searchbar } from '../Searchbar/Searchbar';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { ScrollToTop } from '../ButtonScrollToTop/ButtonScrollToTop';
 
-export class App extends Component {
-  state = {
-    recValue: '',
+export const App = () => {
+  const [recValue, setRecValue] = useState('');
+
+  const formSubmitHandler = request => {
+    setRecValue(request);
   };
 
-  formSubmitHandler = request => {
-    this.setState({ recValue: request });
-  };
+  return (
+    <>
+      <Searchbar onSubmit={formSubmitHandler} />
+      <ImageGallery recValue={recValue} />
 
-  render() {
-    const { recValue } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.formSubmitHandler} />
-        <ImageGallery recValue={recValue} />
+      <ScrollToTop />
 
-        <ScrollToTop />
-
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </>
-    );
-  }
-}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+};
